90a98122ef2c029b806dcd7da25490818ee7cc71
Sam Ward <sam@wordnerds.ai>
2024-11-24 12:05:09 +0000

    update App.vue to use Composition API

--- src/App.vue
+++ src/App.vue
@@ -192,7 +192,7 @@
    </main>
 </template>
 
-<script>
+<script setup>
 import ResumeSection from "./components/ResumeSection.vue";
 import SectionHeadline from "./components/SectionHeadline.vue";
 import Contact from "./components/Contact.vue";
@@ -204,174 +204,169 @@ import PercentageInput from "./components/PercentageInput.vue";
 import SelectInput from "./components/SelectInput.vue";
 import ImgUpload from "./components/ImgUpload.vue";
 import ExportPdf from "./components/ExportPdf.vue";
+import { ref, computed } from "vue";
 
-export default {
-   components: {
-      ResumeSection,
-      SectionHeadline,
-      Contact,
-      EditButtons,
-      ToggleSwitch,
-      Sidebar,
-      ColorInput,
-      PercentageInput,
-      SelectInput,
-      ImgUpload,
-      ExportPdf,
+const colors = ref({
+   left: {
+      highlight: "#82c0cc",
+      text: "#ffffff",
+      background: "#3943b7",
    },
-   data() {
-      return {
-         colors: {
-            left: {
-               highlight: "#82c0cc",
-               text: "#ffffff",
-               background: "#3943b7",
-            },
-            right: {
-               highlight: "#3943b7",
-               text: "#000505",
-               background: "#ffffff",
-            },
-         },
-         name: "Sam Ward",
-         title: "Senior Software Engineer",
-         introText: "From data cleaning to data analysis to machine learning, I am passionate about everything data.",
-         imageUrl: "/profile_pic.jpg",
-         headlines: ["About me", "Contact", "Skills", "Certifications", "Experience", "Education"],
-         contact: {
-            phone: "15713909584",
-            email: "contact@gmail.com",
-            address: "Main St 100, 19777 NY",
-         },
-         skills: ["Python", "Pandas", "SQL", "R", "AI", "C++", "Machine Learning", "Hadoop", "TensorFlow", "PyTorch", "NLP"],
-         highlights: ["Natural Language Processing with Python (Coursera)", "Recommendation Systems with TensorFlow on GCP (Google)"],
-         experience: [
-            {
-               title: "Senior Data Scientist",
-               company: "ABC Analytics Inc.",
-               location: "London",
-               date: "2022 - Present",
-               description: [
-                  "Led a team of data scientists in developing advanced machine learning models for predictive analytics",
-                  "Designed and implemented a recommendation system that boosted cross-selling, leading to a 20% increase in revenue",
-                  "Conducted A/B testing and statistical analysis to optimize product features",
-               ],
-            },
-            {
-               title: "Data Scientist",
-               company: "XYZ Data Solutions",
-               location: "London",
-               date: "2017 - 2019",
-               description: [
-                  "Developed and deployed machine learning models for fraud detection, reducing fraudulent transactions by 18%",
-                  "Conducted in-depth exploratory data analysis to identify key trends and insights",
-                  "Worked on data preprocessing, feature engineering, and model selection to improve model performance",
-               ],
-            },
-            {
-               title: "Data Scientist Trainee",
-               company: "Data Insights Ltd.",
-               location: "New York City",
-               date: "2016-2017",
-               description: ["Collaborated with external partners to integrate third-party data sources, expanding the company's data assets and enhancing predictive modeling capabilities."],
-            },
-         ],
-         education: [
-            {
-               title: "Master of Science in Data Science",
-               university: "StellarTech University",
-               location: "Starville",
-               date: "2020-2022",
-               description: [
-                  "Coursework included advanced machine learning, statistical modeling, and data visualization techniques.",
-                  "Thesis: 'Predictive Modeling for Customer Churn in E-commerce using Random Forest.'",
-               ],
-            },
-            {
-               title: "Bachelor of Science in Computer Science",
-               university: "Evergreen State University",
-               location: "Springdale",
-               date: "2012-2015",
-               description: ["Relevant coursework in database management, algorithms, and programming languages.", "Senior project: 'Development of a Recommender System for Movie Ratings.'"],
-            },
-         ],
-         editing: true,
-         widthLeft: 30,
-         imageShape: "round",
-         headlineWeight: "400",
-         showImage: true,
-         resumeFormat: "a4",
-      };
+   right: {
+      highlight: "#3943b7",
+      text: "#000505",
+      background: "#ffffff",
    },
-   computed: {
-      cssVariables() {
-         return {
-            "--highlight-color-left": this.colors.left.highlight,
-            "--background-color-left": this.colors.left.background,
-            "--text-color-left": this.colors.left.text,
-            "--highlight-color-right": this.colors.right.highlight,
-            "--background-color-right": this.colors.right.background,
-            "--text-color-right": this.colors.right.text,
-            "--headline-weight": this.headlineWeight,
-         };
-      },
-      percentageWidthLeft() {
-         return this.widthLeft + "%";
-      },
+});
+const name = ref("Sam Ward");
+const title = ref("Senior Software Engineer");
+const introText = ref("From data cleaning to data analysis to machine learning, I am passionate about everything data.");
+const imageUrl = ref("/profile_pic.jpg");
+const headlines = ref(["About me", "Contact", "Skills", "Certifications", "Experience", "Education"]);
+const contact = ref({
+   phone: "15713909584",
+   email: "contact@gmail.com",
+   address: "Main St 100, 19777 NY",
+});
+const skills = ref(["Python", "Pandas", "SQL", "R", "AI", "C++", "Machine Learning", "Hadoop", "TensorFlow", "PyTorch", "NLP"]);
+const highlights = ref(["Natural Language Processing with Python (Coursera)", "Recommendation Systems with TensorFlow on GCP (Google)"]);
+const experience = ref([
+   {
+      title: "Senior Data Scientist",
+      company: "ABC Analytics Inc.",
+      location: "London",
+      date: "2022 - Present",
+      description: [
+         "Led a team of data scientists in developing advanced machine learning models for predictive analytics",
+         "Designed and implemented a recommendation system that boosted cross-selling, leading to a 20% increase in revenue",
+         "Conducted A/B testing and statistical analysis to optimize product features",
+      ],
    },
-   methods: {
-      updateHeadline(newValue, index) {
-         this.headlines[index] = newValue;
-      },
-      updateProperty(event, key) {
-         this[key] = event.target.innerText;
-      },
-      updateNestedProperty(event, key1, key2) {
-         this[key1][key2] = event.target.innerText;
-      },
-      updateExperience(event, key, index) {
-         this.experience[index][key] = event.target.innerText;
-      },
-      updateExperienceDescription(event, index1, index2) {
-         this.experience[index1]["description"][index2] = event.target.innerText;
-      },
-      updateEducation(event, key, index) {
-         this.education[index][key] = event.target.innerText;
-      },
-      updateEducationDescription(event, index1, index2) {
-         this.education[index1]["description"][index2] = event.target.innerText;
-      },
-      addExperience() {
-         this.experience.unshift({
-            title: "Job Title",
-            company: "Company",
-            location: "Location",
-            date: "date range",
-            description: ["description"],
-         });
-      },
-      addEducation() {
-         this.education.unshift({
-            title: "Education title",
-            university: "University",
-            location: "Location",
-            date: "date range",
-            description: ["Summa cum laude, GPA 1.0"],
-         });
-      },
-      removeExperience(index) {
-         this.experience.splice(index, 1);
-      },
-      removeEducation(index) {
-         this.education.splice(index, 1);
-      },
-      toggleEditMode(isChecked) {
-         this.editing = isChecked;
-      },
-      toggleImageDisplay(isChecked) {
-         this.showImage = isChecked;
-      },
+   {
+      title: "Data Scientist",
+      company: "XYZ Data Solutions",
+      location: "London",
+      date: "2017 - 2019",
+      description: [
+         "Developed and deployed machine learning models for fraud detection, reducing fraudulent transactions by 18%",
+         "Conducted in-depth exploratory data analysis to identify key trends and insights",
+         "Worked on data preprocessing, feature engineering, and model selection to improve model performance",
+      ],
    },
+   {
+      title: "Data Scientist Trainee",
+      company: "Data Insights Ltd.",
+      location: "New York City",
+      date: "2016-2017",
+      description: ["Collaborated with external partners to integrate third-party data sources, expanding the company's data assets and enhancing predictive modeling capabilities."],
+   },
+]);
+
+const education = ref([
+   {
+      title: "Master of Science in Data Science",
+      university: "StellarTech University",
+      location: "Starville",
+      date: "2020-2022",
+      description: [
+         "Coursework included advanced machine learning, statistical modeling, and data visualization techniques.",
+         "Thesis: 'Predictive Modeling for Customer Churn in E-commerce using Random Forest.'",
+      ],
+   },
+   {
+      title: "Bachelor of Science in Computer Science",
+      university: "Evergreen State University",
+      location: "Springdale",
+      date: "2012-2015",
+      description: ["Relevant coursework in database management, algorithms, and programming languages.", "Senior project: 'Development of a Recommender System for Movie Ratings.'"],
+   },
+]);
+
+const editing = ref(true);
+const widthLeft = ref(30);
+const imageShape = ref("round");
+const headlineWeight = ref("400");
+const showImage = ref(true);
+const resumeFormat = ref("a4");
+
+const cssVariables = computed(() => {
+   return {
+      "--highlight-color-left": colors.value.left.highlight,
+      "--background-color-left": colors.value.left.background,
+      "--text-color-left": colors.value.left.text,
+      "--highlight-color-right": colors.value.right.highlight,
+      "--background-color-right": colors.value.right.background,
+      "--text-color-right": colors.value.right.text,
+      "--headline-weight": headlineWeight.value,
+   };
+});
+
+const percentageWidthLeft = computed(() => {
+   return widthLeft.value + "%";
+});
+
+const updateHeadline = (newValue, index) => {
+   headlines.value[index] = newValue;
+};
+
+const updateProperty = (event, key) => {
+   this[key] = event.target.innerText;
+};
+
+const updateNestedProperty = (event, key1, key2) => {
+   this[key1][key2] = event.target.innerText;
+};
+
+const updateExperience = (event, key, index) => {
+   experience.value[index][key] = event.target.innerText;
+};
+
+const updateExperienceDescription = (event, index1, index2) => {
+   experience.value[index1]["description"][index2] = event.target.innerText;
+};
+
+const updateEducation = (event, key, index) => {
+   education.value[index][key] = event.target.innerText;
+};
+
+const updateEducationDescription = (event, index1, index2) => {
+   education.value[index1]["description"][index2] = event.target.innerText;
+};
+
+const addExperience = () => {
+   experience.unshift({
+      title: "Job Title",
+      company: "Company",
+      location: "Location",
+      date: "date range",
+      description: ["description"],
+   });
+};
+
+const addEducation = () => {
+   education.unshift({
+      title: "Education title",
+      university: "University",
+      location: "Location",
+      date: "date range",
+      description: ["Summa cum laude, GPA 1.0"],
+   });
+};
+
+const removeExperience = (index) => {
+   experience.value.splice(index, 1);
+};
+
+const removeEducation = (index) => {
+   education.value.splice(index, 1);
+};
+
+const toggleEditMode = (isChecked) => {
+   editing.value = isChecked;
+};
+
+const toggleImageDisplay = (isChecked) => {
+   showImage.value = isChecked;
 };
 </script>
 
